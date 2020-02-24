031489d877cc2c2eb74a01a4966c5e0ab706480c
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-02-24 10:23:47 -0500

    Test commit to see if S3 still works after too many pings

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Tony's React App
         </p>
         <p>
-          This confirms deployment on merging PR into master.
+          This confirms deployment on merging PR into master. If this appears, then S3 still works on 200224.
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
