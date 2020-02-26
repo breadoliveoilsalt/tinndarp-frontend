7ef31cd24031c5a0940dee4909cc3bd5bd869288
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-02-26 10:56:22 -0500

    Remove comments from index.js and  unneeded css from App.css

--- src/components/App.js
+++ src/components/App.js
@@ -4,27 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Tony's React App
-        </p>
-        <p>
-          This confirms deployment on merging PR into master. If this appears, then S3 still works on 200224.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 
 import React from 'react'
-// from React: import ReactDOM from 'react-dom';
 import { render } from 'react-dom'
 import './index.css';
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './components/App';
-// From Redux: import App from './components/App'
 import rootReducer from './reducers/rootReducer'
 import * as serviceWorker from './serviceWorker';
 
@@ -21,7 +19,4 @@ render(
   document.getElementById('root')
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
