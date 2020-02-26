30634fd014280e14d9e6a0d96d83e5a7271da935
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-02-26 14:00:16 -0500

    Add first test for App

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,18 +2,22 @@ import React from "react"
 import { render } from "@testing-library/react"
 import App from "./App"
 import Header from "./Header"
-import { shallow } from "enzyme"
+import Enzyme, { shallow } from "enzyme"
+import Adapter from 'enzyme-adapter-react-16'
+Enzyme.configure({ adapter: new Adapter() })
 
 describe("<App />", () => {
   it("is true", () => {
     expect(true).toBeTruthy()
   })
-})
 
-  // it("renders a <Header /> component") {
-  //   const wrapper = shallow(<App />)
-  //   expect(wrapper.find(Header)).to.have.lengthOf(1)
-  // };
+  it("renders a <Header /> component", () => {
+    const wrapper = shallow(<App />)
+    console.log(wrapper.debug())
+    expect(wrapper.find("div")).toHaveLength(1)
+    // expect(wrapper.find(Header)).to.have.lengthOf(1)
+  })
+})
 // test('renders learn react link', () => {
 //   const { getByText } = render(<App />);
 //   const linkElement = getByText(/learn react/i);
