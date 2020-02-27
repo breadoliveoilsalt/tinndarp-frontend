509e275b214662f378bc17411aea6a8c5eb1c11b
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-02-27 14:09:01 -0500

    Delete test featureReducers

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,6 @@
 import { combineReducers } from "redux"
-import feature1Reducer from "features/feature1Reducer"
-import feature2Reducer from "features/feature2Reducer"
 import itemsToBrowseReducer from "../features/browsing/itemsToBrowseSlice"
 
 export default combineReducers({
-  feature1: feature1Reducer,
-  feature2: feature2Reducer,
   itemsToBrowse: itemsToBrowseReducer
 })
