2acd4f66bd4b4d7a151e2e3dc760f1b207b8e3d2
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-02 17:57:39 -0500

    Add failing test for processDataFromAPI()

--- src/api/backendAPIRequests.js
+++ src/api/backendAPIRequests.js
@@ -2,6 +2,17 @@ import * as config from './backendAPIRequestsConfig'
 
 export async function getItems() {
   const url = config.baseURL + "/items"
-  let { data } = await config.fetchWrapper.get(url)
-  return data
+  let { rawData } = await config.fetchWrapper.get(url)
+  return processFromBackendAPI(rawData)
+}
+
+export function processFromBackendAPI(rawData) {
+  return {
+    name: rawData.name,
+    imageURL: rawData.image_url,
+    description: rawData.description,
+    price: rawData.price,
+    moreInfoURL: rawData.more_info_url
+  }
+
 }
