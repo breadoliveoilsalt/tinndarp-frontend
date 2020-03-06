15782a2070892a8b0b002cd7d32136a72c95a734
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-06 16:13:26 -0500

    Add stub routes for home page and browse page

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import Header from './Header'
+import Home from './Home'
 import BrowsingContainer from '../features/browsing/BrowsingContainer'
 import Footer from './Footer'
 import './App.css'
 
 const App = () => {
   return (
-    <div className="app">
-      <Header />
-      <BrowsingContainer />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+          <Switch>
+            <Route path="/browse">
+              <BrowsingContainer />
+            </ Route>
+            <Route path="/">
+              <Home />
+            </ Route>
+          </ Switch>
+        <Footer />
+      </div>
+    </ BrowserRouter>
   )
 }
 
