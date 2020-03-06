c891998eeddee2bae517325dd74c9881c9a873b8
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-06 16:20:38 -0500

    Add stub CreateAccountContainer

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import Header from './Header'
 import Home from './Home'
+import CreateAccountContainer from '../features/userAccount/CreateAccountContainer'
 import BrowsingContainer from '../features/browsing/BrowsingContainer'
 import Footer from './Footer'
 import './App.css'
@@ -12,12 +13,19 @@ const App = () => {
       <div className="app">
         <Header />
           <Switch>
+
+            <Route path="/sign_up">
+              <CreateAccountContainer />
+            </ Route>
+
             <Route path="/browse">
               <BrowsingContainer />
             </ Route>
+
             <Route path="/">
               <Home />
             </ Route>
+
           </ Switch>
         <Footer />
       </div>
---
+++ src/features/userAccount/CreateAccountContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateAccountContainer = () => {
+  return (
+    <div className="home-text"> Sign Up for an Account! </ div>
+  )
+}
+
+export default CreateAccountContainer
