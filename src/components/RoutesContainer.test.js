b0707e2ae6233fd2e7c66b8ac32d070da9992c03
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-07 13:46:49 -0500

    Add and pass RoutesContainer test on rendering Home

--- src/components/RoutesContainer.test.js
+++ src/components/RoutesContainer.test.js
@@ -1,36 +1,40 @@
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
+import Enzyme, { shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 Enzyme.configure({ adapter: new Adapter() })
 import { MemoryRouter, Switch, Route } from 'react-router-dom'
 import RoutesContainer from './RoutesContainer'
+import Home from './Home'
 
-describe("<Routes />", function() {
+describe("<Routes />", () => {
 
-  it("renders the Switch Component", function(){
+  it("renders <Switch />", () => {
     const wrapper = shallow(<RoutesContainer />)
 
     expect(wrapper.find(Switch)).toHaveLength(1)
   })
 
-  it("renders the Switch component with three Route components on a shallow render", function() {
+  it("renders <Switch /> with different possible <Routes />, given a shallow rendering", () => {
     const wrapper = shallow(<RoutesContainer />)
 
     expect(wrapper.find(Route).length).toBeGreaterThan(1)
   })
 
-  // it("renders only the SearchLayoutAndLogic component when the route is '/'", function(){
-  //
-  //   let store = configureStore()
-  //
-  //   const wrapper = mount(
-  //     <Provider store={store} >
-  //       <MemoryRouter exact initialEntries={[ '/' ]}>
-  //         <Routes />
-  //       </MemoryRouter>
-  //     </ Provider>
-  //   )
-  //
+  it("renders only a <Route /> with <Home /> when the route is '/'", () => {
+
+    const wrapper = mount(
+      <MemoryRouter exact initialEntries={[ '/' ]}>
+        <RoutesContainer />
+      </MemoryRouter>
+    )
+
+    const routeComponent = wrapper.find(Route)
+    const homeComponent = wrapper.find(Home)
+    expect(routeComponent.children().length).toEqual(1)
+    expect(routeComponent.children()).toEqual(homeComponent)
+
+  })
+
   //   expect(wrapper.find(SearchLayoutAndLogic)).to.have.lengthOf(1)
   //   expect(wrapper.find(AboutPage)).to.have.lengthOf(0)
   //   expect(wrapper.find(PageNotFound)).to.have.lengthOf(0)
