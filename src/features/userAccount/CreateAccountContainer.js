75e5fc52df75ba80d809d3f4d290f8c99854f07e
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-09 11:18:19 -0400

    Add conditional content rendering to CreateAccountContainer

--- src/features/userAccount/CreateAccountContainer.js
+++ src/features/userAccount/CreateAccountContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from "react-router-dom";
 import AccountForm from './AccountForm'
 import ErrorsDisplay from '../apiRequests/ErrorsDisplay'
 import { submitCreateAccount } from './userAccountSlice'
@@ -24,13 +25,24 @@ export class CreateAccountContainer extends Component {
 
   render() {
 
-    let content = (
-      <div>
-        <div className="large-text">Sign Up for an Account! </ div>
-        {this.props.errors ? <ErrorsDisplay /> : null}
-        <AccountForm action={this.handleCreateAccount} />
-      </div>
-    )
+    let content
+
+    if (this.props.loggedIn) {
+      content = (
+        <div>
+          <div className="large-text">You're already logged in! Please click below to start browsing items.</ div>
+          <Link className="link" to="/browse">Start browsing!</Link>
+        </div>
+      )
+    } else {
+       content = (
+          <div>
+            <div className="large-text">Sign Up for an Account! </ div>
+            {this.props.errors ? <ErrorsDisplay /> : null}
+            <AccountForm action={this.handleCreateAccount} />
+          </div>
+        )
+    }
 
     return content
   }
@@ -40,6 +52,7 @@ export class CreateAccountContainer extends Component {
 const mapStateToProps = (state) => {
   return {
     errors: state.apiRequest.errors,
+    loggedIn: state.userAccount.loggedIn
   }
 }
 
