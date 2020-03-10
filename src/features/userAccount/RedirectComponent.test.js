735bf16d3f6ad9bdfe20671780aed3c94387b1d9
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-10 13:44:03 -0400

    Add RedirectComponent failing test on text rendering

---
+++ src/features/userAccount/RedirectComponent.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Enzyme, { shallow, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+Enzyme.configure({ adapter: new Adapter() })
+import RedirectComponent from './RedirectComponent'
+
+describe("<RedirectComponent />", () => {
+
+  it("renders text passed down as a text prop", () => {
+    const props={text: "You are being redirected!"}
+    const wrapper = shallow(<RedirectComponent {...props} />)
+
+    exect(wrapper.text()).toEqual(props.text)
+  })
+})
