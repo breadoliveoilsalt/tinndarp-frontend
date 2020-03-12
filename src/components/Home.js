314d51732851af61a146b654f38f8fd8c7c7deda
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-12 14:54:00 -0400

    Refactor Home to render account buttons based on log in status

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { loggedInWithToken } from '../features/userAccount/userAccountSlice'
 import Logo from './Logo'
 
-const Home = () => {
+export class Home extends Component {
 
-  return (
-    <div>
-      <div className="large-text">Welcome to</ div>
+  render() {
+    let content
 
-        <Logo className="home-logo"/>
+    if (this.props.loggedIn || loggedInWithToken()) {
+      content = (
+        <div>
 
-    </ div>
-  )
+          <div className="large-text">Nice to see you again! Click below to browse items</div>
+          <button className="action-button logo-style-button" onClick={() => this.props.history.push("/browse")}>Browse</button>
+        </div>
+      )
+    } else {
+      content = (
+       <div>
+         <div className="large-text">Please log in or sign up.</div>
+         <button className="action-button logo-style-button" onClick={() => this.props.history.push("/log_in")}>Log In</button>
+         <button className="action-button logo-style-button" onClick={() => this.props.history.push("/sign_up")}>Sign Up</button>
+      </div>
+      )
+    }
+
+    return (
+      <div>
+        <div className="large-text">Welcome to</div>
+          <Logo className="home-logo"/>
+          {content}
+      </div>
+    )
+  }
+
 }
 
-export default Home
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: state.userAccount.loggedIn
+  }
+}
+
+export default connect(mapStateToProps)(withRouter(Home))
