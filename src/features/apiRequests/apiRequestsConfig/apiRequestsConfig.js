f62f4d67061b5eeb8db5cc044f029966252602a7
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-19 13:57:27 -0400

    Change baseURL to localhost Rails api

--- src/features/apiRequests/apiRequestsConfig/apiRequestsConfig.js
+++ src/features/apiRequests/apiRequestsConfig/apiRequestsConfig.js
@@ -1,4 +1,5 @@
 import axiosWrapper from '../axiosWrapper'
 
-export const baseURL = "https://tinndarp-backend.herokuapp.com/api"
+// export const baseURL = "https://tinndarp-backend.herokuapp.com/api"
+export const baseURL = "http://localhost:3001/api"
 export const fetchWrapper = axiosWrapper
