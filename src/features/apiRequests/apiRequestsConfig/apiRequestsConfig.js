17a0a6e249c4839387317b9626553ec90bbe19fd
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-19 14:33:58 -0400

    Revert base url to Heroku

--- src/features/apiRequests/apiRequestsConfig/apiRequestsConfig.js
+++ src/features/apiRequests/apiRequestsConfig/apiRequestsConfig.js
@@ -1,5 +1,5 @@
 import axiosWrapper from '../axiosWrapper'
 
-// export const baseURL = "https://tinndarp-backend.herokuapp.com/api"
-export const baseURL = "http://localhost:3001/api"
+export const baseURL = "https://tinndarp-backend.herokuapp.com/api"
+// export const baseURL = "http://localhost:3001/api"
 export const fetchWrapper = axiosWrapper
