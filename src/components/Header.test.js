a08dca6f65883a15ab68895b195c3a83b3c1db72
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-03-20 16:30:00 -0400

    Refactor Header tests in light of rendering NavBar

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,31 +1,30 @@
 import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-Enzyme.configure({ adapter: new Adapter() })
 import Header from './Header'
-import Logo from './Logo'
-import Divider from './Divider'
+import HeaderLogoLink from './HeaderLogoLink'
+import NavBar from './NavBar'
 import AccountStatusHeader from '../features/userAccount/AccountStatusHeader'
+
+Enzyme.configure({ adapter: new Adapter() })
 
 describe("<Header />", () => {
 
-  it("renders <Logo /> with a 'logo-header' className", () => {
+  it("renders <HeaderLogoLink />", () => {
     const wrapper = shallow(<Header />)
 
-    expect(wrapper.find(Logo)).toHaveLength(1)
-    expect(wrapper.find(Logo).hasClass("logo-header")).toBeTruthy()
-  })
-
-  it("renders <Divider /> with a 'divider' className", () => {
-    const wrapper = shallow(<Header />)
-
-    expect(wrapper.find(Divider)).toHaveLength(1)
-    expect(wrapper.find(Divider).hasClass("divider")).toBeTruthy()
+    expect(wrapper.find(HeaderLogoLink)).toHaveLength(1)
   })
 
   it("renders <AccountStatusHeader />", () => {
     const wrapper = shallow(<Header />)
 
     expect(wrapper.find(AccountStatusHeader)).toHaveLength(1)
+  })
+
+  it("renders <NavBar />", () => {
+    const wrapper = shallow(<Header />)
+
+    expect(wrapper.find(NavBar)).toHaveLength(1)
   })
 })
